692a44d8ba0308918adb1719f9e683fb1ff84415
Javier Delgado <javier_180796@hotmail.com>
2019-03-03 12:08:57 -0400

    Second Commit
    Front-End (html) and Server Start. JERP!!!

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Entities = require('html-entities').AllHtmlEntities;
+var entities = new Entities();
+
+var port = 8000;
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(port, function(){
+  console.log('Lisening... The port is:'  + port);
+  console.log('Waiting For Players...');
+});
