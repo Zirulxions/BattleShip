5795b5bc1099414818227e8a03588a231f1f02ab
Javier Delgado <javier_180796@hotmail.com>
2019-03-07 10:51:47 -0400

    Fifteenth Commit
    Game in back end updates. FORCED

--- app/game.js
+++ app/game.js
@@ -16,6 +16,34 @@ BattleshipGame.prototype.getPlayerId = function(player) {
   return this.players[player].id;
 };
 
+//Winner
+BattleshipGame.prototype.getWinnerId = function() {
+  if(this.winningPlayer === null) {
+    return null;
+  }
+  return this.players[this.winningPlayer].id;
+};
+
+//Loser
+BattleshipGame.prototype.getLoserId = function() {
+  if(this.winningPlayer === null) {
+    return null;
+  }
+  var loser = this.winningPlayer === 0 ? 1 : 0;
+  return this.players[loser].id;
+};
+
+//Player Turns
+BattleshipGame.prototype.switchPlayer = function() {
+  this.currentPlayer = this.currentPlayer === 0 ? 1 : 0;
+};
+
+BattleshipGame.prototype.abortGame = function(player) {
+  // Opponent win
+  this.gameStatus = GameStatus.gameOver;
+  this.winningPlayer = player === 0 ? 1 : 0;
+}
+
 BattleshipGame.prototype.shoot = function(position) {
   var opponent = this.currentPlayer === 0 ? 1 : 0, gridIndex = position.y * Settings.gridCols + position.x;
   if(this.players[opponent].shots[gridIndex] === 0 && this.gameStatus === GameStatus.inProgress) {
@@ -41,6 +69,15 @@ BattleshipGame.prototype.getPlayerId = function(player) {
 BattleshipGame.prototype.switchPlayer = function() {
   this.currentPlayer = this.currentPlayer === 0 ? 1 : 0;
 };
+
+//Some Settings
+BattleshipGame.prototype.getGameState = function(player, gridOwner) {
+  return {
+      turn: this.currentPlayer === player,                 // is it this player's turn?
+      gridIndex: player === gridOwner ? 0 : 1,             // which client grid to update (0 = own, 1 = opponent)
+      grid: this.getGrid(gridOwner, player !== gridOwner)  // hide unsunk ships if this is not own grid
+    };
+}
 
 BattleshipGame.prototype.getGrid = function(player, hideShips) {
   return {
